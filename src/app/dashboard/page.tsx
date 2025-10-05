04a2d75d5e9ee4e548b9e2b7994663732b3667c0
Andrey-Alohin <andriealohin2020@outlook.com>
2025-10-05 19:21:03 +0300

    base routing

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <main>
+      <h1 className="text-lg">Dashboard Page</h1>
+    </main>
+  );
+}
---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,7 @@
+export default function Settings() {
+  return (
+    <main>
+      <h1 className="text-lg">Settings Page</h1>
+    </main>
+  );
+}
